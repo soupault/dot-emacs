3b61b98787ad8b398ee6ba4e9675caf2093f2831
Egor Panfilov <multicolor.mood@gmail.com>
2016-08-07 11:31:40 +0300

    Load env variables for graphic mode (ocp-indent fix)

--- core/core-env.el
+++ core/core-env.el
@@ -1,6 +1,14 @@
 ;;; core-env.el ---
 
 
+;; Export env variables if loaded from graphic mode
+(use-package exec-path-from-shell
+  :ensure t
+  :init
+  (if (display-graphic-p)
+      (exec-path-from-shell-initialize)))
+
+
 ;; Reduce the frequency of garbage collection by making it happen on
 ;; each 10MB of allocated data (the default is on every 0.76MB)
 (setq gc-cons-threshold 10000000)
